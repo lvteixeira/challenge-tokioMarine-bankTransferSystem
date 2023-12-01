f0d35d694677464a4d61c76b1c1574350f91dc18
Leonardo Viana Teixeira <leoviana73.lv@outlook.com>
2023-11-30 21:04:46 -0300

    .

--- bankTransferSystemApi/bankTransferSystemApi/src/main/java/com/acme/bankTransferSystemApi/repository/TransferRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acme.bankTransferSystemApi.repository;
-
-import com.acme.bankTransferSystemApi.model.TransferEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransferRepository extends JpaRepository<TransferEntity, Long> {
-}
